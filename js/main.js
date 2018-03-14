342303a9ddf13a9f7c87789c64d2f7fcf0999cc3
Davo <master.sport.17@mail.ru>
2018-03-14 16:13:23 +0400

    'Animated

--- js/main.js
+++ js/main.js
@@ -152,3 +152,20 @@ jQuery(document).ready(function($){
 		$newWord.removeClass('is-hidden').addClass('is-visible');
 	}
 });
+
+     $(function() {
+	   if(jQuery('#parallax').length != 0){
+		if(jQuery(window).width() > 980){
+			
+			//apply parallax effect
+			jQuery('#parallax .parallax-layer').parallax(
+				{mouseport: jQuery('#parallax')}
+			);
+		
+			$(window).load(function(){
+				jQuery('#parallax .parallax-layer').css('display','block');		
+				
+			});
+		}
+	}
+ }); 

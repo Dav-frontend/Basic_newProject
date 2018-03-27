21db148f55764d7cda3b462afde4e8c7dad95b8b
Davo <master.sport.17@mail.ru>
2018-03-28 01:14:08 +0400

    'slider'

--- js/basic.js
+++ js/basic.js
@@ -1,34 +1,10 @@
-var timer = 6000;
+$("#slideshow > div:gt(0)").hide();
 
-var i = 0;
-var max = $('#c > a').length;
-
-	$("#c > a").eq(i).addClass('active').css('left','0');
-	$("#c > a").eq(i + 1).addClass('active').css('left','25%');
-	$("#c > a").eq(i + 2).addClass('active').css('left','50%');
-	$("#c > a").eq(i + 3).addClass('active').css('left','75%');
-
-
-	setInterval(function(){
-
-		$("#c > a").removeClass('active');
-
-		$("#c > a").eq(i).css('transition-delay','0.25s');
-		$("#c > a").eq(i + 1).css('transition-delay','0.5s');
-		$("#c > a").eq(i + 2).css('transition-delay','0.75s');
-		$("#c > a").eq(i + 3).css('transition-delay','1s');
-
-		if (i < max-4) {
-			i = i+4;
-		}
-
-		else {
-			i = 0;
-		}
-
-		$("#c > a").eq(i).css('left','0').addClass('active').css('transition-delay','1.25s');
-		$("#c > a").eq(i + 1).css('left','25%').addClass('active').css('transition-delay','1.5s');
-		$("#c > a").eq(i + 2).css('left','50%').addClass('active').css('transition-delay','1.75s');
-		$("#c > a").eq(i + 3).css('left','75%').addClass('active').css('transition-delay','2s');
-
-	}, timer);
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+}, 5000);

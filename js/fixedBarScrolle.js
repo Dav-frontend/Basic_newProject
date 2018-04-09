2708ee3c1acb6ee600481186d44197ef25862c07
Davo <master.sport.17@mail.ru>
2018-04-09 20:58:23 +0400

    'menuBgColor'

--- js/fixedBarScrolle.js
+++ js/fixedBarScrolle.js
@@ -10,6 +10,10 @@ $(window).on('scroll', function() {
         var topBar = document.getElementById('scrollsdown')
         var logoPerson = document.getElementById("logoPerson")
         var logoSearch = document.getElementById('logoSearch')
+        var phoneNumber = document.getElementById('phoneNumber')
+        var logoMenu1 = document.querySelector('.bar1');
+        var logoMenu2 = document.querySelector('.bar2');
+        var logoMenu3 = document.querySelector('.bar3')
         // Your percentage is contained in perc variable
 
         console.log('The percentage is '+perc);
@@ -21,27 +25,47 @@ $(window).on('scroll', function() {
               logo.src = 'img/logo/logoBasic1.png';
               logoSearch.src = 'img/logo/search copy 2.png';
               logoPerson.src = 'img/logo/person 1_ white.png';
+              phoneNumber.style.color = '#e7e8ea';
+              logoMenu1.style.backgroundColor = '#e7e8ea';
+              logoMenu2.style.backgroundColor = '#e7e8ea';
+              logoMenu3.style.backgroundColor = '#e7e8ea'
         }else if(perc >= 13.7 && perc < 27.8){
           topBar.classList.remove("remove");
           topBar.classList.add("active");
               logo.src = 'img/logo/logoBasic2.png';
               logoSearch.src = 'img/logo/search copy 4.png';
               logoPerson.src = 'img/logo/person 2_ white.png';
+              phoneNumber.style.color = 'black';
+              logoMenu1.style.backgroundColor = 'black';
+              logoMenu2.style.backgroundColor = 'black';
+              logoMenu3.style.backgroundColor = 'black';
         }else if(perc >= 27.8 && perc < 42){
           topBar.classList.remove("remove");
           logo.src = 'img/logo/logoBasic1.png';
           logoSearch.src = 'img/logo/search copy 2.png';
           logoPerson.src = 'img/logo/person 1_ white.png';
+          phoneNumber.style.color = '#e7e8ea';
+          logoMenu1.style.backgroundColor = '#e7e8ea';
+          logoMenu2.style.backgroundColor = '#e7e8ea';
+          logoMenu3.style.backgroundColor = '#e7e8ea'
         }else if(perc >= 42 && perc < 56){
           topBar.classList.remove("remove");
           logo.src = 'img/logo/logoBasic2.png';
           logoSearch.src = 'img/logo/search copy 4.png';
           logoPerson.src = 'img/logo/person 2_ white.png';
+          phoneNumber.style.color = 'black';
+          logoMenu1.style.backgroundColor = 'black';
+          logoMenu2.style.backgroundColor = 'black';
+          logoMenu3.style.backgroundColor = 'black';
         }else if(perc >= 56 && perc < 71){
           topBar.classList.remove("remove");
           logo.src = 'img/logo/logoBasic1.png';
           logoSearch.src = 'img/logo/search copy 2.png';
           logoPerson.src = 'img/logo/person 1_ white.png';
+          phoneNumber.style.color = '#e7e8ea';
+          logoMenu1.style.backgroundColor = '#e7e8ea';
+          logoMenu2.style.backgroundColor = '#e7e8ea';
+          logoMenu3.style.backgroundColor = '#e7e8ea'
         }else if(perc >= 71 ){
           topBar.classList.add("remove");
         }

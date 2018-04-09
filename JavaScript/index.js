c17a97698611e0ba934e9bcfa4d0e680ac7c35da
Davo <master.sport.17@mail.ru>
2018-04-10 02:36:39 +0400

    mediaScrole

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -32,20 +32,9 @@
 
 
   $(window).on('scroll', function() {
-             var st = $(this).scrollTop();
-             var wh = $(document).height();
 
-          // st : wh = X : 100
-          // x = (st*100)/wh
-
-          var perc = (st*100)/wh
-
-          // Your percentage is contained in perc variable
-
-
-          if(perc >= 30 ){
-            callbackFunc();
-          }
+              callbackFunc();
+          
 
           });
 })();

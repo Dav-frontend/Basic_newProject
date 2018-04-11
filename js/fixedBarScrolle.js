51e0c8f0b997f0d73c130d596ba11a7568fa3e72
Davo <master.sport.17@mail.ru>
2018-04-11 18:16:16 +0400

    topBarScrolle

--- js/fixedBarScrolle.js
+++ js/fixedBarScrolle.js
@@ -2,4 +2,17 @@
 //
 //
 //
-//         });
+//  });
+
+window.addEventListener("scroll", topBar);
+var bar = document.getElementById('scrollsDown');
+
+function topBar(){
+  if (document.documentElement.scrollTop > 200) {
+    bar.classList.add("active");
+
+  }else if( document.documentElement.scrollTop < 200) {
+    bar.classList.remove("active");
+
+  }
+}
